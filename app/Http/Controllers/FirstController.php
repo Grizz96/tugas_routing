57f08b06f51bb202b1383a8eb0f1a6545466e0a4
Grizz96 <121376755+Grizz96@users.noreply.github.com>
2024-02-21 21:52:37 +0700

    first commit

---
+++ app/Http/Controllers/FirstController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FirstController extends Controller
+{
+    public function nyoba(){
+        echo "Dimana ada cahaya disitu ada saya";
+    }
+
+   
+}
